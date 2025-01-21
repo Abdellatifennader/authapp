520794fa723388ce901790df8c2a67d5fd6c640e
Abdellatif <abdellatifennader8@gmail.com>
2025-01-21 21:39:20 +0100

    header

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,49 +1,34 @@
 import Link from "next/link";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-import {
-  
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenu
-   
-  } from "@/components/ui/navigation-menu"
-  import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
-  
-  const Header = () => {
+export default function Header() {
     return (
-        <div className="flex items-center justify-center pt-10 ">
-        <NavigationMenu >
-        <NavigationMenuItem >
-        <Link href="/" legacyBehavior passHref >
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Home
-          </NavigationMenuLink>
-        </Link>
-        <Link href="/about" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            About
-          </NavigationMenuLink>
-        </Link>
-       <SignedIn>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-          <UserButton/>
-          </NavigationMenuLink>
-          </SignedIn>
-
-       
-          <SignedOut>
-     
-            <SignInButton/>
-      
-          </SignedOut>
-          
-        
-      </NavigationMenuItem>
-      </NavigationMenu>
-      </div>
-    )
+      <header className='bg-white flex items-center justify-center'>
+        <div className='max-w-6xl mx-auto flex justify-between mx-20 items-center pt-3'>
+          {/* logo */}
+          <Link href='/' className='text-2xl  text-gray-500 font-extrabold group cursor-pointer'>
+            <span className='text-transparent bg-clip-text '>
+              Auth
+            </span>
+            <span className='text-transparent bg-clip-text text-gray-500'>
+              App
+            </span>
+          </Link>
+          {/* add a navigation menu */}
+          <nav>
+            <ul className='flex gap-24'>
+              <Link href='/'>Home</Link>
+              <Link href='/about'>About</Link>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
   }
-  
-  export default Header
   
