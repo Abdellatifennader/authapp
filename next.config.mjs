2f9748c41b9d9a7a77df7718c548d155d55e3ed2
Abdellatif <abdellatifennader8@gmail.com>
2025-01-21 23:50:15 +0100

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-      // Custom Webpack rules here
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
