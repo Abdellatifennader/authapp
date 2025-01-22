571853ac35c72584900578723fd3f722ed369f85
Abdellatif <abdellatifennader8@gmail.com>
2025-01-22 12:08:40 +0100

    mongoose

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -2,24 +2,23 @@ import mongoose from "mongoose";
 
 let initialized = false;
 
-export const connect = async () =>{
+export const connect = async () => {
+  mongoose.set('strictQuery', true);
 
-    mongoose.set('strictQuery',true);
-    if(initialized){
-        console.log('MongoDB already connected');
-        return;
+  if (initialized) {
+    console.log('MongoDB already connected');
+    return;
+  }
 
-    }
-    try{
-        await mongoose.connect(process.env.MOGODB_URI,{
-            dbName:"next auth app",
-            useNewUrlParser:true,
-            useUnifieldTopology:true,
-        })
-        console.log('MongoDB connected');
-        initialized=true;
-    }catch(error){
-
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'next-auth-app',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+    initialized = true;
+  } catch (error) {
+    console.log('MongoDB connection error:', error);
+  }
+};
