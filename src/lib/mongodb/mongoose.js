55843856e5c5b1a5beab7ce13021afe050595da0
Abdellatif <abdellatifennader8@gmail.com>
2025-01-22 12:38:30 +0100

    db

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -14,7 +14,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: 'next-auth-app',
+      dbName: 'authapp',
       
     });
     console.log('MongoDB connected');
